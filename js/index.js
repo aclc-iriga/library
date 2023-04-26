bf5e0f90f5a2b170893128e5e7b1b6aac38dc577
kulotsystems <kulotsystems@gmail.com>
2023-04-27 07:50:02 +0800

    Remove toString method.

--- js/index.js
+++ js/index.js
@@ -11,14 +11,14 @@ const app = Vue.createApp({
             let q = this.query.toLowerCase();
             this.found = this.data.filter((d) => {
                 if(Array.isArray(d)) {
-                    return d[0].toString().toLowerCase().includes(q)
-                        || d[1].toString().toLowerCase().includes(q)
-                        || d[2].toString().toLowerCase().includes(q)
-                        || d[3].toString().toLowerCase().includes(q)
-                        || d[4].toString().toLowerCase().includes(q)
-                        || d[5].toString().toLowerCase().includes(q)
-                        || d[6].toString().toLowerCase().includes(q)
-                        || d[7].toString().toLowerCase().includes(q)
+                    return d[0].toLowerCase().includes(q)
+                        || d[1].toLowerCase().includes(q)
+                        || d[2].toLowerCase().includes(q)
+                        || d[3].toLowerCase().includes(q)
+                        || d[4].toLowerCase().includes(q)
+                        || d[5].toLowerCase().includes(q)
+                        || d[6].toLowerCase().includes(q)
+                        || d[7].toLowerCase().includes(q)
                 }
                 return false;
             });
